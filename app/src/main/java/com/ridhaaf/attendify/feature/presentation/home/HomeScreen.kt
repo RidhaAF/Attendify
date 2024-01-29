f72c2350973ab692b43445ad008888b750b501e5
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2024-01-29 11:06:38 +0700

    feat: add locale time section

--- app/src/main/java/com/ridhaaf/attendify/feature/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/ridhaaf/attendify/feature/presentation/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.ridhaaf.attendify.feature.presentation.home
 
+import android.icu.text.SimpleDateFormat
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -23,16 +24,26 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.ridhaaf.attendify.feature.presentation.components.DefaultButton
 import com.ridhaaf.attendify.feature.presentation.components.DefaultPhotoProfile
+import com.ridhaaf.attendify.feature.presentation.components.DefaultSpacer
 import com.ridhaaf.attendify.feature.presentation.components.defaultToast
 import com.ridhaaf.attendify.feature.presentation.routes.Routes
+import kotlinx.coroutines.delay
+import java.util.Locale
 
 @OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -77,15 +88,23 @@ fun HomeScreen(
     ) {
         Box(
             modifier = modifier
+                .fillMaxSize()
                 .pullRefresh(pullRefreshState)
                 .verticalScroll(verticalScrollState)
                 .padding(it),
+            contentAlignment = Alignment.Center,
         ) {
             Column(
                 modifier = modifier
                     .fillMaxSize()
                     .padding(16.dp),
-            ) {}
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                ClockSection()
+                DefaultSpacer(size = 8)
+                ClockInOutButton()
+            }
             PullRefreshIndicator(
                 refreshing = refreshing,
                 state = pullRefreshState,
@@ -148,4 +167,55 @@ private fun SignOutButton(
             navController?.navigate(Routes.SIGN_IN)
         }
     }
+}
+
+@Composable
+private fun ClockSection() {
+    var currentTime by remember { mutableStateOf(getCurrentTime()) }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            currentTime = getCurrentTime()
+            delay(1000)
+        }
+    }
+
+    Text(
+        currentTime,
+        fontSize = 48.sp,
+        fontWeight = FontWeight.SemiBold,
+    )
+}
+
+@Composable
+private fun ClockInOutButton() {
+    val clockInTime = isBetween("08:00:00", "17:00:00")
+    val clockOutTime = isBetween("17:00:00", "22:00:00")
+    val idle = !isBetween("08:00:00", "22:00:00")
+
+    val enabled = !idle
+
+    DefaultButton(
+        onClick = {},
+        enabled = enabled,
+    ) {
+        val buttonText = when {
+            clockInTime -> "Clock In"
+            clockOutTime -> "Clock Out"
+            else -> "Clock In"
+        }
+
+        Text(buttonText)
+    }
+}
+
+private fun getCurrentTime(): String {
+    val currentTime = System.currentTimeMillis()
+    val dateFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+    return dateFormat.format(currentTime)
+}
+
+private fun isBetween(startTime: String, endTime: String): Boolean {
+    val currentTime = getCurrentTime()
+    return currentTime in startTime..endTime
 }
